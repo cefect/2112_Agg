52f6e11215e7f6406344ef6dd1db14aecb6546ab
cefect <cefect@gmail.com>
2022-04-12 09:20:30 +0200

    rest finv_agg trues

---
+++ tests/hyd/tests_integ.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr. 12, 2022
+
+@author: cefect
+
+integration test for hyd.model
+'''
+
+#test a single run call w/ all 3 study areas
+
+
+
+
+
+
+#test all runs configured in the modelPars.xls
