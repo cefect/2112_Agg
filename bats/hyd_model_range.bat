db98eda03ec7b6d7a14cce10afa16874fa9bfc4f
cefect <cefect@gmail.com>
2022-04-18 18:14:19 +0200

    Update hyd_model_range.bat

--- bats/hyd_model_range.bat
+++ bats/hyd_model_range.bat
@@ -3,7 +3,7 @@ REM SET ids=(90,91,92)
 SET START=76
 SET COUNT=3
 
-SET NAME=hyd6_dev
+SET NAME=hyd6
 
 call C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\bats\hyd_setup.bat
 
@@ -11,7 +11,7 @@ REM execute
 SET /a "END=%START%+%COUNT%"
 @echo on
 FOR /l %%i IN (%START%,1,%END%) DO (
-python C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\agg\hyd\main.py %%i -n %NAME% -d
+python C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\agg\hyd\main.py %%i -n %NAME% 
 )
 
 FOR /l %%i IN (%START%,1,%START%+%COUNT%) DO (
