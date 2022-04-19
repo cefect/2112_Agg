b6ae4f4f8e29feef0152ffeb782aab2755572919
cefect <cefect@gmail.com>
2022-04-19 16:06:45 +0200

    bats

--- bats/hyd_model_range_parallel.bat
+++ bats/hyd_model_range_parallel.bat
@@ -1,16 +1,16 @@
 REM paramters
-SET ids=(94,95)
+SET ids=(1,2)
 REM ,)
 
 SET LAG=1
-SET NAME=hyd6
+SET NAME=hyd7
 
 call C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\bats\hyd_setup.bat
 
 REM execute (using ping command to add some lag between calls)
 @echo on
 FOR %%i IN %ids% DO (
-start cmd /k python C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\agg\hyd\main.py %%i -n %NAME%
+start cmd /k python -O C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\agg\hyd\main.py %%i -n %NAME%
 ping 127.0.0.1 -n %LAG% > nul
 )
 
