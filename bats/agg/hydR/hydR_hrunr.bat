85057e1a13c2e122076d9cc36448b1359ba1478a
cefect <47092034+cefect@users.noreply.github.com>
2022-05-08 11:04:43 +0200

    Update hydR_hrunr.bat

--- bats/agg/hydR/hydR_hrunr.bat
+++ bats/agg/hydR/hydR_hrunr.bat
@@ -1,11 +1,11 @@
  
-call C:\LS\09_REPOS\02_JOBS\2112_Agg\src\bats\setup.bat
+call C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\bats\setup.bat
 
 REM execute
  
 @echo on
  
-python -O C:\LS\09_REPOS\02_JOBS\2112_agg\src\agg\hydR\hr_runr.py
+python -O C:\LS\09_REPOS\02_JOBS\2112_agg\cef\agg\hydR\hr_runr.py
  
 ECHO finished 
 
