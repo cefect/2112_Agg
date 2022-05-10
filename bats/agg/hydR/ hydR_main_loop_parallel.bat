a3475643bfac5b71d5f65e9a301564ab80c4d348
cefect <47092034+cefect@users.noreply.github.com>
2022-05-10 17:26:17 +0200

    update runners

---
+++ bats/agg/hydR/ hydR_main_loop_parallel.bat
@@ -0,0 +1,19 @@
+SET DS_L=(pre, post, preGW)
+
+SET LAG=1
+
+
+REM execute
+call C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\bats\setup.bat
+@echo on
+FOR %%i IN %DS_L% DO (
+start cmd /k python -O C:\LS\09_REPOS\02_JOBS\2112_agg\cef\agg\hydR\main.py -t %%i -n hr6 -i 7 -dsampStage %%i
+ping 127.0.0.1 -n %LAG% > nul
+)
+ECHO finished 
+
+pause
+
+ 
+
+
--- bats/agg/hydR/hydR_main.bat
+++ bats/agg/hydR/hydR_main.bat
@@ -5,7 +5,7 @@ REM execute
  
 @echo on
  
-python -O C:\LS\09_REPOS\02_JOBS\2112_agg\cef\agg\hydR\main.py -t wse -n hr5 -i 2
+python -O C:\LS\09_REPOS\02_JOBS\2112_agg\cef\agg\hydR\main.py -t wse -n hr5 -i 8
  
 ECHO finished 
 
