eb4d0930de2c66ffe7cf21b20aa8ab322f768d43
cefect <47092034+cefect@users.noreply.github.com>
2022-05-10 18:18:36 +0200

    Update hydR_main.bat

--- bats/agg/hydR/hydR_main.bat
+++ bats/agg/hydR/hydR_main.bat
@@ -1,11 +1,13 @@
- 
+
+SET dsampStage=post
+
 call C:\LS\09_REPOS\02_JOBS\2112_Agg\cef\bats\setup.bat
 
 REM execute
  
 @echo on
  
-python -O C:\LS\09_REPOS\02_JOBS\2112_agg\cef\agg\hydR\main.py -t wse -n hr5 -i 8
+python -O C:\LS\09_REPOS\02_JOBS\2112_agg\cef\agg\hydR\main.py -t %dsampStage% -n hr6 -i 7 -dsampStage %dsampStage%
  
 ECHO finished 
 
