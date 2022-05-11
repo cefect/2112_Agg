520af53632b1eb1e7ce10166006e4e0fd50d4ac4
cefect <47092034+cefect@users.noreply.github.com>
2022-05-11 13:13:04 +0200

    Update prep.py
    RandomSA

--- agg/prep.py
+++ agg/prep.py
@@ -3,22 +3,110 @@ Created on May 9, 2022
 
 @author: cefect
 '''
-import os
-from qgis.core import QgsCoordinateReferenceSystem
+import os, sys
+import numpy as np
+from qgis.core import QgsCoordinateReferenceSystem, QgsMapLayerStore
 from hp.hyd import HQproj
+from agg.hyd.hscripts import StudyArea
+from definitions import proj_lib, base_resolution
+np.random.seed(100)
 
+class RandomSA(StudyArea, HQproj):
+    def __init__(self,
+                 resolution=None,
+                 **kwargs):
+        
+        if resolution is None:
+            resolution=base_resolution
+        self.resolution=resolution
+        
+        super().__init__(base_resolution=resolution, **kwargs)
+        
+        
 
-def build_random( #construct a random project
-        base_proj='obwb'
+        
+        self.set_extent(self.dem_fp_d[base_resolution])
+        
+    def set_extent(self,
+                    raw_fp,
+                    logger=None,
+                    ):
+        if logger is None: logger=self.logger
+        log=logger.getChild('set_extents')
+        #=======================================================================
+        # set extents
+        #=======================================================================
+        mstore = QgsMapLayerStore()
+        raw_rlay = self.get_layer(raw_fp, mstore=mstore)
+        
+        assert self.rlay_get_resolution(raw_rlay)==self.resolution
+        
+        #=======================================================================
+        # wrap
+        #=======================================================================
+        self.extent=raw_rlay.extent()
+        log.info('set extent from %s'%raw_rlay.name())
+        mstore.removeAllMapLayers()
+        
+        return self.extent
+    
+    def build_rand(self, 
+                   resolution=None,
+                   out_dir=None,
+                   layname='layer',
+                   logger=None,
+                   **kwargs):
+        if logger is None: logger=self.logger
+        log=logger.getChild('build_rand')
+        if resolution is None: resolution=self.resolution
+        if out_dir is None: out_dir=self.out_dir
+        
+        fp =  self.randomuniformraster(resolution,  extent=self.extent,
+                             output=os.path.join(out_dir, '%s.tif'%layname), **kwargs)
+        
+        log.info('built %s'%fp)
+        return self.get_layer(fp)
+        
+        
+        
+        
+        
+
+def build_random_proj( #construct a random project
+        base_proj='obwb',
+        
+        #init kwargs
+        init_kwargs = dict(trim=False,overwrite=True),
+
         ):
-    pass
+ 
+    print('from %s'%base_proj)
+    
+ 
+    
+    with RandomSA(tag='rand', name='prep',
+                   **init_kwargs, **proj_lib[base_proj]) as ses:
+        
+        #get the random layers
+        dem = ses.build_rand(layname='dem_'+ses.longname)
+        
+        wse_raw = ses.build_rand(layname='wse_raw_'+ses.longname)
+        
+        #gw filter the wse
+        wsegw = ses.wse_remove_gw(wse_raw, dem_rlay=dem)
+        
+        print(wsegw.source())
+        
+        
+        
+ 
 
 def convert_wse(
         out_dir=r'C:\LS\10_OUT\2112_Agg\outs\prep\0510',
         compression='med',
         studyArea_l=None,
         ):
-    from definitions import proj_lib, base_resolution
+    
     
     if not os.path.exists(out_dir):os.makedirs(out_dir)
     
@@ -49,5 +137,6 @@ def convert_wse(
         
 
 if __name__ == "__main__":
-    convert_wse(studyArea_l=['obwb'])
+    build_random_proj()
+    #convert_wse(studyArea_l=['obwb'])
     print('finished')
