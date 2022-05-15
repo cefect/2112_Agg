42c74978753fb7245970d40d3b38007dbc4146ed
cefect <cefect@gmail.com>
2022-05-15 09:59:50 +0200

    create hydR_coms

---
+++ agg/hydR/hydR_coms.py
@@ -0,0 +1,279 @@
+'''
+Created on May 15, 2022
+
+@author: cefect
+'''
+
+class Catalog(object): #handling the simulation index and library
+    df=None
+    keys = ['resolution', 'studyArea', 'downSampling', 'dsampStage', 'severity',
+            'aggType', 'samp_method', 'aggLevel']
+    cols = ['dkey', 'stat']
+ 
+
+    
+    def __init__(self, 
+                 catalog_fp='fp', 
+                 logger=None,
+                 overwrite=True,
+                 index_col=list(range(5)),
+                 ):
+        
+        if logger is None:
+            import logging
+            logger = logging.getLogger()
+ 
+        #=======================================================================
+        # attachments
+        #=======================================================================
+        self.logger = logger.getChild('cat')
+        self.overwrite=overwrite
+        self.catalog_fp = catalog_fp
+        
+        
+        #mandatory keys
+        """not using this anymore"""
+        self.cat_colns = []
+        
+        #=======================================================================
+        # load existing
+        #=======================================================================
+        if os.path.exists(catalog_fp):
+            self.df = pd.read_csv(catalog_fp, 
+                                  index_col=index_col,
+                                  header = [0,1],
+                                  )
+            self.check(df=self.df.copy())
+            self.df_raw = self.df.copy() #for checking
+            
+        else:
+            self.df_raw=pd.DataFrame()
+        
+    def clean(self):
+        raise Error('check consitency between index and library contents')
+    
+    def check(self,
+              df=None,
+              ):
+        #=======================================================================
+        # defai;lts
+        #=======================================================================
+        log = self.logger.getChild('check')
+        if df is None: df = self.df.copy()
+        log.debug('on %s'%str(df.shape))
+        
+        
+        
+        #=======================================================================
+        # #check columns
+        #=======================================================================
+        assert not None in df.columns.names
+        miss_l = set(self.cols).symmetric_difference(df.columns.names)
+        assert len(miss_l)==0, miss_l
+        
+        assert isinstance(df.columns, pd.MultiIndex)
+        
+ 
+        #=======================================================================
+        # #check index
+        #=======================================================================
+        assert not None in df.index.names
+        #=======================================================================
+        # assert df[self.idn].is_unique
+        # assert 'int' in df[self.idn].dtype.name
+        #=======================================================================
+        assert isinstance(df.index, pd.MultiIndex)
+        
+        miss_l = set(df.index.names).difference(self.keys)
+        assert len(miss_l)==0, miss_l
+ 
+        
+        #check filepaths
+        errs_d = dict()
+        
+        bx_col = df.columns.get_level_values(1).str.endswith('fp')
+        assert bx_col.any()
+        
+        for coln, cserx in df.loc[:, bx_col].items():
+ 
+            
+            for id, path in cserx.items():
+                if pd.isnull(path):
+                    log.warning('got null filepath on %s'%str(id))
+                    continue
+                if not os.path.exists(path):
+                    errs_d['%s_%s'%(coln, id)] = path
+                    
+        if len(errs_d)>0:
+            log.error(errs_d)
+            raise Error('got %i/%i bad filepaths'%(len(errs_d), len(df)))
+ 
+ 
+        
+    
+    def get(self):
+        assert os.path.exists(self.catalog_fp), self.catalog_fp
+        self.check()
+        return self.df.copy().sort_index()
+    
+    
+    def remove(self, keys_d,
+               logger=None): #remove an entry
+        #=======================================================================
+        # defaults 
+        #=======================================================================
+        if logger is None: logger=self.logger
+        log=logger.getChild('remove')
+        df_raw = self.df.copy()
+        
+        #identify this row
+        bx = pd.Series(True, index=df_raw.index)
+        for k,v in keys_d.items():
+            bx = np.logical_and(bx,
+                                df_raw.index.get_level_values(k)==v,
+                                )
+            
+        assert bx.sum()==1
+        
+        #remove the raster
+        """no... this should be handled by the library writer
+        rlay_fp = df_raw.loc[bx, 'rlay_fp'].values[0]
+        
+        os.remove(rlay_fp)"""
+            
+        
+        #remove the row
+        self.df = df_raw.loc[~bx, :]
+        
+        log.info('removed %s'%(keys_d))
+        
+ 
+
+    def add_entry(self,
+                  serx,
+                  keys_d={},
+                  logger=None,
+                  ):
+        """
+        cat_d.keys()
+        """
+        #=======================================================================
+        # defaults
+        #=======================================================================
+        if logger is None: logger=self.logger
+        log=logger.getChild('add_entry')
+        cat_df = self.df
+        
+        """making this more flexible
+        keys = self.keys.copy()"""
+        keys = list(keys_d.keys())
+
+        log.debug('w/ %i'%len(serx))
+        #check mandatory columns are there
+        miss_l = set(self.cat_colns).difference(serx.index.get_level_values(1))
+        assert len(miss_l)==0, 'got %i missing cols: %s'%(len(miss_l), miss_l)
+        
+        for k in keys: 
+            assert k in keys_d
+        
+        #=======================================================================
+        # prepare new 
+        #=======================================================================
+        
+        #new_df = pd.Series(cat_d).rename(keys_d.values()).to_frame().T
+ 
+        new_df = serx.to_frame().T
+        new_df.index = pd.MultiIndex.from_tuples([keys_d.values()], names=keys_d.keys())
+        """
+        view(new_df)
+        """
+        #=======================================================================
+        # append
+        #=======================================================================
+        if cat_df is None:
+            #convet the series (with tuple name) to a row of a multindex df
+            cat_df=new_df
+        else:
+            #check if present
+            
+            cdf = cat_df.index.to_frame().reset_index(drop=True)
+            ndf = new_df.index.to_frame().reset_index(drop=True)
+ 
+            bxdf = cdf.apply(lambda s:ndf.eq(s).iloc[0,:], axis=1)
+            bx = bxdf.all(axis=1)
+ 
+            
+            
+            if bx.any():
+                
+                #===============================================================
+                # remomve an existing entry
+                #===============================================================
+                assert bx.sum()==1
+                assert self.overwrite
+                bx.index = cat_df.index
+                self.remove(dict(zip(keys, cat_df.loc[bx, :].iloc[0,:].name)))
+                
+                cat_df = self.df.copy()
+                
+            
+            #===================================================================
+            # append
+            #===================================================================
+            cat_df = cat_df.append(new_df,  verify_integrity=True)
+            
+        #=======================================================================
+        # wrap
+        #=======================================================================
+        self.df = cat_df
+ 
+        
+        log.info('added %s'%len(keys_d))
+ 
+        
+    def __enter__(self):
+        return self
+    def __exit__(self, *args, **kwargs):
+        if self.df is None: 
+            return
+        #=======================================================================
+        # write if there was a change
+        #=======================================================================
+        if not np.array_equal(self.df, self.df_raw):
+            log = self.logger.getChild('__exit__')
+            df = self.df
+            
+            #===================================================================
+            # delete the old for empties
+            #===================================================================
+ 
+            if len(df)==0:
+                try:
+                    os.remove(self.catalog_fp)
+                    log.warning('got empty catalog... deleteing file')
+                except Exception as e:
+                    raise Error(e)
+            else:
+                #===============================================================
+                # write the new
+                #===============================================================
+                """should already be consolidated... just overwrite"""
+ 
+                df.to_csv(self.catalog_fp, mode='w', header = True, index=True)
+                self.logger.info('wrote %s to %s'%(str(df.shape), self.catalog_fp))
+        
+#===============================================================================
+# funcs
+#===============================================================================
+def assert_lay_lib(lib_d, msg=''):
+    if __debug__:
+        assert isinstance(lib_d, dict)
+        for k0,d0 in lib_d.items():
+            if not isinstance(d0, dict):
+                raise AssertionError('bad subtype on %s: %s\n'%(
+                    k0, type(d0))+msg)
+            
+            for k1, lay in d0.items():
+                if not isinstance(lay, QgsMapLayer):
+                    raise AssertionError('bad type on %s.%s: %s\n'%(
+                        k0,k1, type(lay))+msg)
