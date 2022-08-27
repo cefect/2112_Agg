6d111c6d404e9b6da6d60b4d8252bb814e231dbd
cefect <cefect@gmail.com>
2022-08-27 16:45:36 +0200

    Update test_haz.py

--- tests2/haz/test_haz.py
+++ tests2/haz/test_haz.py
@@ -10,3 +10,24 @@ import numpy as np
 import pytest, copy, os, random
 import rasterio as rio
 xfail = pytest.mark.xfail
+
+
+#===============================================================================
+# helpers and globals------
+#===============================================================================
+crsid = 2953
+prec=5
+
+#for test data
+output_kwargs = dict(crs=rio.crs.CRS.from_epsg(crsid),
+                     transform=rio.transform.from_origin(1,100,1,1)) 
+
+ 
+@pytest.fixture(scope='function')
+def dem_fp(dem_ar, tmp_path): 
+    return write_array(dem_ar, os.path.join(tmp_path, 'dem.tif'), **output_kwargs)
+ 
+
+@pytest.fixture(scope='function')
+def wse_fp(wse_ar, tmp_path): 
+    return write_array(wse_ar, os.path.join(tmp_path, 'wse.tif'), **output_kwargs)
