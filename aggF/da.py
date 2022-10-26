e5c42ac5412d536ee496e25e87e0f322907722fd
Seth Bryant <bryant.seth@gmail.com>
2022-10-26 22:20:47 +0200

    start plot_matrix_rlDelta_xb

--- aggF/da.py
+++ aggF/da.py
@@ -8,7 +8,11 @@ Created on Oct. 26, 2022
 from aggF.scripts import AggSession1F
 
  
-    
+class Plot_rlDelta_xb(object):
+    def plot_matrix_rlDelta_xb(self,serx):
+        """matrix plot of all functions rlDelta vs. xb"""
+        log = self.logger.getChild('p')
+           
         
-class Session_AggF(AggSession1F):
+class Session_AggF(AggSession1F, Plot_rlDelta_xb):
     pass
